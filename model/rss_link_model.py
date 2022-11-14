8892dff703d3b107ea36930b5862c29c39905489
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-13 20:17:16 -0400

    Update rss_link_model.py

--- model/rss_link_model.py
+++ model/rss_link_model.py
@@ -1,6 +1,15 @@
 
 from pydantic import BaseModel
+from deta import Deta
 from typing import List
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+PROJECT_KEY = os.environ.get("PROJECT_KEY")
+
+deta = Deta(PROJECT_KEY)
+users = deta.Base("users")
 
 
 class RssLink(BaseModel):
