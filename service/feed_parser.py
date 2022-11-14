78d3f43efffc6318833df3dbcca5c5ab50f54277
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-13 20:17:05 -0400

    Create feed_parser.py

---
+++ service/feed_parser.py
@@ -0,0 +1,12 @@
+import feedparser
+
+rss_link = "https://carlosmv.hashnode.dev/rss.xml"
+
+
+def list_of_links(link: str):
+    feed = feedparser.parse(link)
+
+    list_links = []
+    for entry in feed['entries']:
+        list_links.append(entry.links[0]['href'])
+    return list_links
