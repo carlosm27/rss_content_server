e9cf4e0ab13f52b1e130de76f82830de0c3ad9e4
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2022-12-25 17:34:13 -0400

    Add Author Class

--- feed_parser.py
+++ feed_parser.py
@@ -1,4 +1,5 @@
 import feedparser
+from datetime import date
 
 rss_link = "https://carlosmv.hashnode.dev/rss.xml"
 
@@ -13,4 +14,25 @@ def list_of_links(link: str):
 
 
 
+class Author:
+    def __init__(self, link):
+        self.link = link
+
+        self.feed = feedparser.parse(self.link)
+        self.entries = self.feed['entries']
+
+    def author_info(self):
+        
+
+        AuthorName = self.entries[0]['author']
+        Number_Entries = len(self.entries)
+        Link = self.link
+        LastEntry = self.entries[0]['title']
+        LastEntryHref = self.entries[0]['link']
+        LastEntryDate = self.entries[0]['published']
+
+        return AuthorName, Link, LastEntry, LastEntryDate, LastEntryHref, Number_Entries   
+
+author = Author(rss_link)
+print(author.author_info())
 
