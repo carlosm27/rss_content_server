486eb2d3566eca6e8c508240b113e09b6d4a8373
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2022-12-25 17:34:41 -0400

    Add Author Class

--- model/rss_link_model.py
+++ model/rss_link_model.py
@@ -4,7 +4,7 @@ from deta import Deta
 from typing import List
 from dotenv import load_dotenv
 import os
-
+from datetime import date
 load_dotenv()
 PROJECT_KEY = os.environ.get("PROJECT_KEY")
 
@@ -15,3 +15,13 @@ users = deta.Base("users")
 class RssLink(BaseModel):
     ChatId: int
     Links: List[str]
+
+class Author:
+    def __init__(self, link):
+
+        AuthorName: str
+        Number_Entries: int
+        self.link = link
+        LastEntry: str
+        LastEntryDate: date
+
